ed9ffdbdc7807e9c6452d779d902e7d0f8caf7ae
mariotmc <mariotomicx@gmail.com>
2022-01-20 19:56:21 +0100

    Add App structure and firebaseConfig

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { initializeApp } from "firebase/app";
+import Header from "./components/Header";
+import Game from "./components/Game";
+import "./styles/style.css";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3iA1MMz_KohWoA85SptLz_vuzch9Fnwo",
+  authDomain: "where-is-waldo-59262.firebaseapp.com",
+  projectId: "where-is-waldo-59262",
+  storageBucket: "where-is-waldo-59262.appspot.com",
+  messagingSenderId: "846905820342",
+  appId: "1:846905820342:web:6da2c9af23d5ead26c5217",
+};
+
+const app = initializeApp(firebaseConfig);
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Header />
+      <Game />
+    </>
+  );
 }
 
 export default App;
