b188a6183a6866ce1066eb68c11769247d5c35b3
mariotmc <mariotomicx@gmail.com>
2022-01-27 18:43:02 +0100

    Move firebase code to own file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import { initializeApp } from "firebase/app";
-import Header from "./components/Header";
 import Game from "./components/Game";
+import "./firebase";
 import "./styles/style.css";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB3iA1MMz_KohWoA85SptLz_vuzch9Fnwo",
-  authDomain: "where-is-waldo-59262.firebaseapp.com",
-  projectId: "where-is-waldo-59262",
-  storageBucket: "where-is-waldo-59262.appspot.com",
-  messagingSenderId: "846905820342",
-  appId: "1:846905820342:web:6da2c9af23d5ead26c5217",
-};
-
-const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
     <>
-      <Header />
       <Game />
     </>
   );
