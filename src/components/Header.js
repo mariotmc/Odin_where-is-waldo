db59f1dd892e7e4a836f70e074876a14b17cd17f
mariotmc <mariotomicx@gmail.com>
2022-01-27 18:40:33 +0100

    Add timer and characters to be found

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,58 @@
-const Header = () => {
+import { useState } from "react";
+
+const Header = (timerComponent, remainingCharactersState, charactersState) => {
+  const timer = timerComponent;
+  const remainingCharacters = remainingCharactersState;
+  const [characters] = charactersState;
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  function getKey() {
+    return Math.floor(Math.random() * 100000);
+  }
+
+  const styles = {
+    textDecoration: "line-through",
+    textDecorationColor: "red",
+    textDecorationThickness: "3px",
+  };
+
   return (
     <header>
-      <h1>findUs</h1>
+      <nav>
+        <ul>
+          <li>
+            <h1>findUs</h1>
+          </li>
+          <li>
+            <div id="timer" style={{ color: "white" }}>
+              {timer}
+            </div>
+          </li>
+          <li>
+            <button
+              id="dropdown-button"
+              onClick={() => (showDropdown === false ? setShowDropdown(true) : setShowDropdown(false))}
+            >
+              {remainingCharacters}
+            </button>
+            {showDropdown && (
+              <div id="dropdown-menu">
+                {characters.map((element) => {
+                  if (element.found === true) {
+                    return (
+                      <h3 key={getKey()} style={styles}>
+                        {element.id}
+                      </h3>
+                    );
+                  } else {
+                    return <h3 key={getKey()}>{element.id}</h3>;
+                  }
+                })}
+              </div>
+            )}
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
