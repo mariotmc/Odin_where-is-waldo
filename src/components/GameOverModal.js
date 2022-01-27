e4260c83019f96ffa726002990fc84b18338c704
mariotmc <mariotomicx@gmail.com>
2022-01-27 18:42:20 +0100

    Add highscore display logic, restart button functionality

--- src/components/GameOverModal.js
+++ src/components/GameOverModal.js
@@ -1,31 +1,66 @@
-const GameOverModal = (props) => {
-  const [setGameOver] = props;
+import { fetchCharacters, getTopScores } from "../firebase";
+
+const GameOverModal = (
+  gameOverState,
+  timerActiveState,
+  timerComponent,
+  secondsState,
+  minutesState,
+  hoursState,
+  setCharactersState,
+  setRemainingCharactersState,
+  highscores
+) => {
+  const [setGameOver] = gameOverState;
+  const [setIsTimerActive] = timerActiveState;
+  const [setSeconds] = secondsState;
+  const [setMinutes] = minutesState;
+  const [setHours] = hoursState;
+  const [setCharacters] = setCharactersState;
+  const [setRemainingCharacters] = setRemainingCharactersState;
+  const timer = timerComponent;
+  const [topScores, setTopScores] = highscores;
+  const topTenScores = topScores.slice(0, 10);
 
   return (
     <div className="modal">
       <div id="gameover-container">
         <div id="time">
           <h1>YOUR TIME</h1>
-          <h2>00:00:00</h2>
+          <h2>{timer}</h2>
         </div>
 
         <div id="highscores">
           <h1>HIGHSCORES</h1>
-          <ul>
-            <li>1. Peter</li>
-            <li>2. Gabriel</li>
-            <li>3. Mark</li>
-            <li>4. John</li>
-            <li>5. Harry</li>
-            <li>6. Chris</li>
-            <li>7. Evan</li>
-            <li>8. Logan</li>
-            <li>9. Mike</li>
-            <li>10. George</li>
-          </ul>
+          <ol style={{ width: "210px" }}>
+            {topTenScores.map((element) => {
+              return (
+                <div className="score-container" style={{ display: "flex" }}>
+                  <li key={Math.floor(Math.random() * 100000)} className="score">
+                    <div className="score-name">{element.name}</div>
+                  </li>
+                  <div className="score-time">
+                    {new Date(element.time * 1000).toISOString().substr(11, 8)}
+                  </div>
+                </div>
+              );
+            })}
+          </ol>
         </div>
 
-        <button className="button" onClick={() => setGameOver(null)}>
+        <button
+          className="button"
+          onClick={() => {
+            setSeconds(0);
+            setMinutes(0);
+            setHours(0);
+            setCharacters(fetchCharacters());
+            setRemainingCharacters(3);
+            setTopScores(getTopScores());
+            setGameOver(null);
+            setIsTimerActive(true);
+          }}
+        >
           RESTART
         </button>
       </div>
